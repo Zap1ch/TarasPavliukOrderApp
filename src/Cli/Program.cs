114d7e6f1db21bdf8dac300a78777e61631da23a
Zap1ch <tarasp41@gmail.com>
2026-09-21 23:53:43 +0300

    lab02: add Core library and multi-targeting

--- src/Cli/Program.cs
+++ src/Cli/Program.cs
@@ -1,46 +1,13 @@
-﻿using System.Runtime.InteropServices;
-using System.Text.Json;
-using System.Text.Encodings.Web;
+﻿using Core;
 
-if (args.Contains("--json"))
-{
-    var info = new
-    {
-        Application = "TarasPavliukOrderApp",
-        Student = "Павлюк Тарас",
-        Group = "ФЕІ-36",
-        OSDescription = RuntimeInformation.OSDescription,
-        OSVersion = Environment.OSVersion.ToString(),
-        ProcessArchitecture = RuntimeInformation.ProcessArchitecture.ToString(),
-        CLRVersion = Environment.Version.ToString(),
-        Runtime = RuntimeInformation.FrameworkDescription,
-        ApplicationDirectory = AppContext.BaseDirectory,
-        CurrentDirectory = Environment.CurrentDirectory,
-        Domain = "Замовлення",
-        Entities = "замовлення, покупець, товар, рядок замовлення"
-    };
+EnvironmentReport report = EnvironmentInfo.Collect();
 
-    var options = new JsonSerializerOptions
-    {
-        Encoder = JavaScriptEncoder.UnsafeRelaxedJsonEscaping
-    };
+Console.WriteLine("TarasPavliukOrderApp – інформація про середовище");
+Console.WriteLine(new string('-', 52));
 
-    Console.WriteLine(JsonSerializer.Serialize(info, options));
-}
-else
-{
-    Console.WriteLine("TarasPavliukOrderApp – практикум з крос-платформного програмування");
-    Console.WriteLine("Студент: Павлюк Тарас, група ФЕІ-36");
-    Console.WriteLine(new string('-', 52));
-
-    Console.WriteLine($"ОС (OSDescription) : {RuntimeInformation.OSDescription}");
-    Console.WriteLine($"ОС (Environment) : {Environment.OSVersion}");
-    Console.WriteLine($"Архітектура процесу : {RuntimeInformation.ProcessArchitecture}");
-    Console.WriteLine($"Версія .NET (CLR) : {Environment.Version}");
-    Console.WriteLine($"Runtime : {RuntimeInformation.FrameworkDescription}");
-    Console.WriteLine($"Каталог застосунку : {AppContext.BaseDirectory}");
-    Console.WriteLine($"Поточний каталог : {Environment.CurrentDirectory}");
-
-    Console.WriteLine(new string('-', 52));
-    Console.WriteLine("Предметна область: Замовлення (замовлення, покупець, товар, рядок замовлення)");
-}
+Console.WriteLine($"ОС : {report.OsDescription}");
+Console.WriteLine($"Runtime : {report.FrameworkDescription}");
+Console.WriteLine($"Архітектура : {report.ProcessArchitecture}");
+Console.WriteLine($"RID (визначено) : {report.DetectedRid}");
+Console.WriteLine($"RID (від .NET) : {report.ReportedRid}");
+Console.WriteLine($"Каталог : {report.BaseDirectory}");
